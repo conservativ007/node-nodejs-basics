489eb0d3deee8a65310933f70b71177c16a18c36
Maks <alisa3d2007@gmail.com>
2022-10-20 00:32:13 +0300

    aadded solution to child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,25 @@
+import { spawn } from "child_process";
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+    let init = false;
+
+    const chProcess =
+        spawn(
+            "node",
+            ["./src/cp/files/script.js", ...args.split(" ")]
+        );
+
+    process.stdin.on("data", (msg) => {
+        chProcess.stdin.write(msg);
+    });
+
+    process.stdout.on("data", (data) => {
+        console.log(data.toString());
+        if (!init) {
+            console.log("Write anything in console..." + "\n");
+            init = true;
+        }
+    });
 };
+
+spawnChildProcess("--silent --all");                    
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,31 @@
+import { cpus } from "os";
+import { Worker } from "worker_threads";
+
+import __dirname from "../fs/__dirname.js";
+
 export const performCalculations = async () => {
-    // Write your code here
+
+    const cp = cpus();
+    let num = 20;
+
+    const workerResults = await Promise.allSettled(cp.map(() => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker("./src/wt/worker.js", {
+                workerData: num++
+            })
+            worker.on("message", msg => resolve(msg));
+            worker.on("error", msg => reject(msg));
+        });
+    }));
+
+    let result = workerResults.map(({ status, value }) => {
+        return {
+            status: status === "fulfilled" ? "resolved" : "error",
+            data: status === "fulfilled" ? value : null
+        }
+    })
+
+    console.log(result);
 };
+
+performCalculations();
